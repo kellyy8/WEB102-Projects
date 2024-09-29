b09f825b7240011ad76bf8d52cf5b79bc052313c
kellyy8 <kellyyu2886@gmail.com>
2024-09-29 16:32:06 -0700

    Set up handler to get a random next question.

--- 2FlashcardsPt1/src/App.jsx
+++ 2FlashcardsPt1/src/App.jsx
@@ -6,19 +6,29 @@ const App = () => {
   const cardPairs = {  // TODO: Update sample card pairs.
     "A" : "a",
     "B" : "b",
+    "C" : "c",
+    "D" : "d",
+    "E" : "e",
   }
   
-  const [question, setQuestion] = useState("B")
+  const [question, setQuestion] = useState("A")
+  const allQuestions = Object.keys(cardPairs)
+  const numCards = allQuestions.length
+
+  const setNextQuestion = () => {  // TODO: Make sure that next card shows question first, even if prev card last showed its answer.
+    const index = Math.floor(Math.random() * numCards)  // Expected output: 0, ..., numPairs-1
+    setQuestion(allQuestions[index])
+  }
 
   return (
     <div>
       <div className="intro">
         <h1>Flashcards</h1>
         <p>Description...</p>
-        <p>Number of cards: {/** TODO */}</p>
+        <p>Number of cards: {numCards}</p>
       </div>
       <Flashcard q={question} a={cardPairs[question]}/>
-      <button onClick={()=>{/** TODO */}}>Next card</button>
+      <button onClick={setNextQuestion}>Next</button>
     </div>
   )
 }
