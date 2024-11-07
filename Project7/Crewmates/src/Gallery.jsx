28a4d775fbe077727088b606692c96ec201a7710
kellyy8 <kellyyu2886@gmail.com>
2024-11-06 17:31:14 -0800

    Created template components and styles for gallery page.

--- Project7/Crewmates/src/Gallery.jsx
+++ Project7/Crewmates/src/Gallery.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './Gallery.css'
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <h1>"Name"</h1>
+            <h2>Attributes: </h2>
+            {/* * Paragraph element for every attribute in the list. */}
+            {/** View profile button. */}
+            {/** Update button. */}
+            {/** Delete button. */}
+        </div>
+    )
+    
+}
 
 const Gallery = () => {
-    
+    return (
+        <div>
+            <h1>Gallery</h1>
+            <div className="galleryContainer">
+                <Profile/>
+                <Profile/>
+                {/* * A list of all the crewmates in the gallery. */}
+                {/* * Each crewmate should be a link to their profile page. */}
+            </div>
+        </div>
+    )
 }
 
 export default Gallery
