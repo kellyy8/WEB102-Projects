e448c5e1b21db6af6fad8414ad0679b4c0b25b59
kellyy8 <kellyyu2886@gmail.com>
2024-11-06 17:04:45 -0800

    Set up navbar with just the homepage link.

--- Project7/Crewmates/src/main.jsx
+++ Project7/Crewmates/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Outlet, Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+const Layout = () => {
+  return (
+    <div>
+      <nav style={{width:"100%"}}>
+        <div className="home-link" key="home-button">
+          <Link style={{ color: "white" }} to="/">
+            Home
+          </Link>
+        </div>
+      </nav>
+      
+      <Outlet/>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index={true} element={<App />} />
+          {/* <Route index={false} path="/catDetails/:symbol" element={<DetailedView/>}/> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
