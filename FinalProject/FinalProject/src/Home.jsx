54bc46a9eb286209b3c0f846f2b9ddf931f789da
kellyy8 <kellyyu2886@gmail.com>
2024-11-18 21:44:22 -0800

    Cleaned up home page styles so all pages fill up the whole screen.

--- FinalProject/FinalProject/src/Home.jsx
+++ FinalProject/FinalProject/src/Home.jsx
@@ -1,17 +1,10 @@
 import './Home.css'
 import './shared_variables.css'
-import CreatePost from './CreatePost'
-import Gallery from './Gallery'
 
 const Home = () => {
-
   return (
     <div>
       <h1>Crafty Cats</h1>
-
-      {/** TODO: Sample content to test components. Update later. */}
-      <CreatePost />
-      <Gallery />
     </div>
   )
 }
