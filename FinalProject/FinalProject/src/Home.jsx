8eaffbb6930b5782e6e8ea5599259020dd813d10
kellyy8 <kellyyu2886@gmail.com>
2024-11-19 16:32:20 -0800

    Updated and styled home page.

--- FinalProject/FinalProject/src/Home.jsx
+++ FinalProject/FinalProject/src/Home.jsx
@@ -1,10 +1,14 @@
 import './Home.css'
 import './shared_variables.css'
+import logo from './images/logo.png'
 
 const Home = () => {
   return (
     <div>
       <h1>Crafty Cats</h1>
+      <p className="description"> This is a place for everyone to share their favorite crafts!</p>
+      <p className="description"> Click on the Gallery tab to view all the posts.</p>
+      <img src={logo} height="400px" alt='Latte art of a cat in a coffee cup. Created with Canva.'/>
     </div>
   )
 }
