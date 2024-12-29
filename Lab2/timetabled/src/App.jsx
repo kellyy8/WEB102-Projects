14b392706b4a382ebe7858fb4942bfb8eefa3ece
kellyy8 <kellyyu2886@gmail.com>
2024-12-29 13:58:10 -0800

    Added Labs 1 and 2 to repo.

---
+++ Lab2/timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> 7-Day Itinerary for Chicago </h1>
+      <h2> Welcome to Chicago! Enjoy your stay! </h2>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
